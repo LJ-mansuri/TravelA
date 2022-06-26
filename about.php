c97531d45b4c940f0fbe3ec198ce40f71804a957
LJ-mansuri <56364836+LJ-mansuri@users.noreply.github.com>
2022-06-26 15:12:04 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php
+include 'includes/header.php';
+?>
+<div class="container-fluid newbg">
+  <div class="row bg-footer text-light text-center">
+  <h1 class="p-3">ABOUT</h1>
+  </div>
+  <div class="row bg-home">
+  <h3 class="p-2 text-center">A FEW WORDS ABOUT US</h3>
+</div>
+  <div class="row">
+   <div class="col-sm-6 p-3">
+       <img src="images\home-gallery-1-1200x800-original.jpg">
+</div>
+<div class="col-sm-6">
+  <p class="p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorum mollitia ex cumque nulla. Unde animi omnis laborum neque fugit tempora, nostrum ut est rerum quia, voluptate quos deleniti labore?</p>
+  </div></div></div>
+</div>
+<div class="row bg-home">
+  <h3 class="p-3">Meet Our Team</h3>
+  </div>
+<div class="row p-5 ">
+<div class="card-deck">
+  <div class="card bg-header text-light">
+    <img src="images/review1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Sophie Robbins</h5>
+      <p class="card-text">Travel Agent</p>
+      </div>
+  </div>
+  <div class="card bg-header text-light">
+    <img src="images/review2.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Ted Howells</h5>
+      <p class="card-text">Tour Guide</p>
+      </div>
+  </div>
+  <div class="card bg-header text-light">
+    <img src="images/review3.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Jannete Brooks</h5>
+      <p class="card-text">Travel Visa Specialist</p>
+      </div>
+  </div>
+</div></div></div>
+<div class="row bg-home">
+  <h3 class="p-3">What You Get With Us</h3>
+  </div>
+  <div class="row p-3">
+  <div class="col-sm-6">
+    <h6>Daily Tours</h6>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorum mollitia ex cumque nulla. Unde animi omnis laborum neque fugit tempora, nostrum ut est rerum quia, voluptate quos deleniti labore?</p>
+  </div>
+  <div class="col-sm-6">
+    <h6>Cruises</h6>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorum mollitia ex cumque nulla. Unde animi omnis laborum neque fugit tempora, nostrum ut est rerum quia, voluptate quos deleniti labore?</p>
+  </div>
+</div>
+<div class="row p-3">
+  <div class="col-sm-6">
+    <h6>Epic Journeys</h6>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorum mollitia ex cumque nulla. Unde animi omnis laborum neque fugit tempora, nostrum ut est rerum quia, voluptate quos deleniti labore?</p>
+  </div>
+  <div class="col-sm-6">
+    <h6>New Destinations</h6>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorum mollitia ex cumque nulla. Unde animi omnis laborum neque fugit tempora, nostrum ut est rerum quia, voluptate quos deleniti labore?</p>
+  </div>
+</div>
+<div class="row p-3">
+  <div class="col-sm-6">
+    <h6>24/7 Online Support</h6>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorum mollitia ex cumque nulla. Unde animi omnis laborum neque fugit tempora, nostrum ut est rerum quia, voluptate quos deleniti labore?</p>
+  </div>
+  <div class="col-sm-6">
+    <h6>Bus Tours</h6>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorum mollitia ex cumque nulla. Unde animi omnis laborum neque fugit tempora, nostrum ut est rerum quia, voluptate quos deleniti labore?</p>
+  </div>
+</div>
+
+    <?php
+include 'includes/footer.php';
+?>
+
+</body>
+</html>
