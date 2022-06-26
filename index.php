ea3b42d9cb870a9a2530e3d8660bbf71e0a3e946
LJ-mansuri <56364836+LJ-mansuri@users.noreply.github.com>
2022-06-26 15:11:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php
+include 'includes/header.php';
+?>
+<?php
+include 'includes/slides.php';
+?>
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-6 col-sm-6 p-3">
+<img src="images/about1.jpg"></div>
+<div class="col-lg-6 col-sm-6 p-2">
+<div class="card border-info mb-3 bg-header" style="max-width: 18rem; ">
+  <div class="card-header">About Travel</div>
+  <div class="card-body text-light">
+    <h5 class="card-title">Explore a different way to travel</h5>
+    <p class="card-text"> Lorem ipsum. Sit, possimus quam laboriosam Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum iusto fugiat, culpa possimus ipsam deleniti exercitationem inventore accusantium corrupti. Nobis esse amet eum nisi rem maxime! Enim, beatae in. Voluptates!</p>
+    <div class="card-footer bg-transparent border-light"><a href="about.php">Read more</a></div>
+  </div></div></div>
+</div>
+</div>
+<div class="container-fluid bg-home">
+<div class="row p-5 ">
+<div class="card-deck">
+  <div class="card">
+    <img src="images/review1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="images/review2.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="images/review3.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div></div></div>
+</div>
+</div>
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
